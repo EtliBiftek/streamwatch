291c6ba6095d14f24fb3f4457ad2c1787842de8b
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-08-31 17:51:14 +0300

    fix: guard legacy stream polling during startup

--- entry.js
+++ entry.js
@@ -3,6 +3,7 @@
 require('./updater-v2.js');
 require('./player-engine-v2.js');
 require('./live-tools.js');
+require('./polling-guard.js');
 require('./enhancements.js');
 require('./developer-polish.js');
 require('./taskbar-icon.js');
