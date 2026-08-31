287650e7853570efc8c176aa857d7d2d7ba616f6
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-08-31 17:49:53 +0300

    feat: enable realtime live tools and player engine v2

--- entry.js
+++ entry.js
@@ -1,7 +1,9 @@
 'use strict';
 
 require('./updater-v2.js');
-require('./player-engine.js');
+require('./player-engine-v2.js');
+require('./live-tools.js');
 require('./enhancements.js');
 require('./developer-polish.js');
 require('./taskbar-icon.js');
+require('./ui-integrations.js');
