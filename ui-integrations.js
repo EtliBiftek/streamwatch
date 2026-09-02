d9a6d131b8798104c22f954a4274a819e4dd1c37
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-09-02 18:50:39 +0300

    feat: inject browser session bridge UI

--- ui-integrations.js
+++ ui-integrations.js
@@ -28,6 +28,7 @@ function inject(win) {
     injectFile(win, 'src/renderer/layout-v2.js', 'js'),
     injectFile(win, 'src/renderer/account-bridge.js', 'js'),
     injectFile(win, 'src/renderer/account-bridge-v3.js', 'js'),
+    injectFile(win, 'src/renderer/browser-session-bridge.js', 'js'),
     injectFile(win, 'src/renderer/interface-polish.js', 'js'),
   ];
   Promise.allSettled(jobs).then((results) => {
