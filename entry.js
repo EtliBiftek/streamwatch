b431a7106734b6a4e5013164e5df72dd18df1468
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-09-02 18:53:47 +0300

    chore: make Browser Bridge the only browser session importer

--- entry.js
+++ entry.js
@@ -10,6 +10,7 @@ require('./local-auth-portal.js');
 require('./account-connections-v2.js');
 require('./account-connections-v3.js');
 require('./browser-session-bridge.js');
+require('./browser-bridge-legacy-off.js');
 require('./enhancements.js');
 require('./developer-polish.js');
 require('./taskbar-icon.js');
